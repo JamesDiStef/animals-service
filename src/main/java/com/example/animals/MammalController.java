dffc9b8278019717f12135b02f539872929fc71d
James <james.distef@gmail.com>
2025-03-27 10:11:53 -0400

    initial

--- src/main/java/com/example/animals/MammalController.java
+++ src/main/java/com/example/animals/MammalController.java
@@ -1,6 +1,7 @@
 package com.example.animals;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("api/v1/mammals")
 @AllArgsConstructor
+@CrossOrigin(origins = "*")
 public class MammalController {
     private final MammalService mammalService;
 
